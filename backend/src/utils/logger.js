95da1076055f7a29c2ed810c3909febeff770556
jameelk <31823682+jameelk@users.noreply.github.com>
2026-04-09 02:36:43 +0300

    Add backend/src/utils/logger.js

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,9 @@
+const fs: require('fs').createWriteStream('logs/app.log');
+
+const logger = {
+  info: (msg) => console.log('[INFO]', msg),
+  error: (msg, err) => console.error('[ERROR]', msg, err),
+  warn: (msg) => console.warn('[WARN]', msg)
+};
+
+module.exports = logger;
