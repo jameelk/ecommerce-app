6b7ed87302ef761445262544166ae26f0be67346
jameelk <31823682+jameelk@users.noreply.github.com>
2026-04-09 02:42:39 +0300

    Add mobile/src/store/wishlistStore.js

---
+++ mobile/src/store/wishlistStore.js
@@ -0,0 +1,1 @@
+import create from 'zustand'; export const wishlistStore = create((set) => ({ items: [], addItem: (item) => set((state) => ({ items: [...state.items, item] })), removeItem: (id) => set((state) => ({ items: state.items.filter(i => i.id !== id) })) }));
