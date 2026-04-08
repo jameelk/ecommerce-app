89c29baa1574e241cbf26908daaacdf5b1c5dd0e
jameelk <31823682+jameelk@users.noreply.github.com>
2026-04-09 02:42:38 +0300

    Add mobile/src/store/cartStore.js

---
+++ mobile/src/store/cartStore.js
@@ -0,0 +1,1 @@
+import create from 'zustand'; export const cartStore = create((set) => ({ items: [], addItem: (item) => set((state) => ({ items: [...state.items, item] })), removeItem: (id) => set((state) => ({ items: state.items.filter() => i.id !== id) })), clearCart: () => set({ items: [] }) }));
